5b2a9e5c086952916d882cc8d3717edcda0da4f6
andrey_velsk <andrey_velsk@mail.ru>
2019-03-31 22:56:10 +0300

    add some blocks

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.response-items').slick(
+        {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        }
+    );
+});
